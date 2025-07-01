cf24e7aa543f46022bf7670f4a1a413ababc5de8
Daniil Antipov <kiki1881@yandex.ru>
2025-07-01 17:50:38 +0300

    🔧 chore(husky + commitlint): try №101 add emojies in my commits

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,3 +1,7 @@
 export default {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-empty': [0, 'never'], // Отключаем проверку пустого subject
+    'type-empty': [0, 'never'], // Отключаем проверку пустого типа
+  },
 };
