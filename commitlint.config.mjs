c23e220c0197db3812180164453ba22ca4247e93
Daniil Antipov <kiki1881@yandex.ru>
2025-07-01 17:51:58 +0300

    style:
    \

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,7 +1,7 @@
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'subject-empty': [0, 'never'], // Отключаем проверку пустого subject
-    'type-empty': [0, 'never'], // Отключаем проверку пустого типа
+    'subject-empty': [0, 'never'],
+    'type-empty': [0, 'never'],
   },
 };
