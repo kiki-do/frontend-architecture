cf454f2fcda09fbbfe35b61e5c557593b3af6aa8
Daniil Antipov <kiki1881@yandex.ru>
2025-07-01 17:03:36 +0300

    add about internalization

--- src/routes/about.lazy.tsx
+++ src/routes/about.lazy.tsx
@@ -1,9 +1,12 @@
 import { createLazyFileRoute } from '@tanstack/react-router';
+import { useTranslation } from 'react-i18next';
 
 export const Route = createLazyFileRoute('/about')({
   component: About,
 });
 
 function About() {
-  return <div className="p-2">Hello from About!</div>;
+  const { t } = useTranslation('', { keyPrefix: 'about-page' });
+
+  return <div className="p-2">{t('title')}</div>;
 }
